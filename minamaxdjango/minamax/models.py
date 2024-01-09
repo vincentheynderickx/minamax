a687eee751d7d5f4dcf39db7ae3fd37647bcf3ce
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 16:01:42 +0100

    cop pas bug

--- minamaxdjango/minamax/models.py
+++ minamaxdjango/minamax/models.py
@@ -1,26 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     points = models.IntegerField(default=0)
+
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     event_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     event_date = models.DateTimeField("date event")
+
     def __str__(self):
         return str(self.id) + " : " + self.event_text + " " + str(self.pub_date) + " "
+
 
 class Possibility(models.Model):
     id = models.AutoField(primary_key=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     description_text = models.CharField(max_length=200)
     quotation = models.FloatField(default=2.0)
-    RESULT_CHOICES = [('Pending', 'Pending'),('Win', 'Win'),('Lose', 'Lose')]
-    result = models.CharField(max_length=10, choices=RESULT_CHOICES, default='Pending')
+    RESULT_CHOICES = [("Pending", "Pending"), ("Win", "Win"), ("Lose", "Lose")]
+    result = models.CharField(max_length=10, choices=RESULT_CHOICES, default="Pending")
+
     def __str__(self):
         return self.event.event_text + " " + self.description_text
+
 
 class Bet(models.Model):
     id = models.AutoField(primary_key=True)
@@ -28,5 +34,6 @@ class Bet(models.Model):
     possibility = models.ForeignKey(Possibility, on_delete=models.CASCADE)
     putting = models.FloatField(default=0.0)
     username = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.event.event_text + " " + " " + str(self.putting)
--- minamaxdjango/minamax/views.py
+++ minamaxdjango/minamax/views.py
@@ -7,23 +7,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    latest_question_list = Event.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
+    latest_event_list = Event.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.event_text for q in latest_event_list])
     return HttpResponse(output)
 
 
-def detail(request, question_id):
+def detail(request, event_id):
     try:
-        question = Event.objects.get(pk=question_id)
+        event = Event.objects.get(pk=event_id)
     except Event.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "minamax/detail.html", {"question": question})
+        raise Http404("Event does not exist")
+    return render(request, "minamax/detail.html", {"question": event})
 
 
 def results(request, event_id):
-    response = "You're looking at the results of question %s."
+    response = "You're looking at the results of event %s."
     return HttpResponse(response % event_id)
-
-
-def vote(request, event_id):
-    return HttpResponse("You're voting on question %s." % event_id)
