1c1201878365c5f0becbf01bb33f19e0cfdb2ac9
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 16:13:48 +0100

    small co

--- minamaxdjango/minamax/views.py
+++ minamaxdjango/minamax/views.py
@@ -3,21 +3,22 @@ from django.http import Http404
 from django.http import HttpResponse
 from .models import Event
 from django.template import loader
-from django.shortcuts import render
 
 
 def index(request):
+    latest_event_list = Event.objects.order_by("-pub_date")[:5]
+    context = {"latest_event_list": latest_event_list}
+    return render(request, "polls/index.html", context)
+
+    # def index(request):
     latest_event_list = Event.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.event_text for q in latest_event_list])
     return HttpResponse(output)
 
 
 def detail(request, event_id):
-    try:
-        event = Event.objects.get(pk=event_id)
-    except Event.DoesNotExist:
-        raise Http404("Event does not exist")
-    return render(request, "minamax/detail.html", {"question": event})
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, "polls/detail.html", {"event": event})
 
 
 def results(request, event_id):
