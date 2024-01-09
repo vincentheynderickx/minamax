63ae4f70b3849ee322d33cc0c6995fc7cf0703a2
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 15:52:11 +0100

    small co

--- minamaxdjango/minamax/views.py
+++ minamaxdjango/minamax/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Event.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
