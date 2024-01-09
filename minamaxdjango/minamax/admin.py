0d8c3615d69b297a5590b95268fb6f48879c161b
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 14:28:33 +0100

    import classes from .models to admin

--- minamaxdjango/minamax/admin.py
+++ minamaxdjango/minamax/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 from .models import Event
+from .models import Possibility
+from .models import Bet
+
 
 admin.site.register(Event)
+admin.site.register(Possibility)
+admin.site.register(Bet)
