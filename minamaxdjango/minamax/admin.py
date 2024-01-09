fdd78949512aebafa5318b5e3fc410f772100e1d
vincentheynderickx <v.heynderickx@gmail.com>
2024-01-09 15:22:33 +0100

    change data-structure

--- minamaxdjango/minamax/admin.py
+++ minamaxdjango/minamax/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import CustomUser
 from .models import Event
 from .models import Possibility
 from .models import Bet
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'points', 'is_staff']
 
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Event)
 admin.site.register(Possibility)
 admin.site.register(Bet)
--- minamaxdjango/minamax/models.py
+++ minamaxdjango/minamax/models.py
@@ -17,6 +17,8 @@ class Possibility(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     description_text = models.CharField(max_length=200)
     quotation = models.FloatField(default=2.0)
+    RESULT_CHOICES = [('Pending', 'Pending'),('Win', 'Win'),('Lose', 'Lose')]
+    result = models.CharField(max_length=10, choices=RESULT_CHOICES, default='Pending')
     def __str__(self):
         return self.event.event_text + " " + self.description_text
 
@@ -25,5 +27,6 @@ class Bet(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     possibility = models.ForeignKey(Possibility, on_delete=models.CASCADE)
     putting = models.FloatField(default=0.0)
+    username = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     def __str__(self):
         return self.event.event_text + " " + self.result.description_text + " " + str(self.putting)
