baf58ab1b4b1595eaf9c438d4b53f0cc1a53f644
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 15:04:02 +0100

    views added in minamax

--- minamaxdjango/minamax/views.py
+++ minamaxdjango/minamax/views.py
@@ -4,3 +4,25 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bets index.")
+
+
+def detail(request, event_id):
+    return HttpResponse("You're looking at question %s." % event_id)
+
+
+def results(request, event_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % event_id)
+
+
+def vote(request, event_id):
+    return HttpResponse("You're voting on question %s." % event_id)
+
+
+from .models import Event
+
+
+def index(request):
+    latest_question_list = Event.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.event_text for q in latest_question_list])
+    return HttpResponse(output)
