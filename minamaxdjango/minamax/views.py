44bd1d5415e48a233e980a9378edaca1371e3745
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 16:15:11 +0100

    smal co

--- minamaxdjango/minamax/views.py
+++ minamaxdjango/minamax/views.py
@@ -8,7 +8,7 @@ from django.template import loader
 def index(request):
     latest_event_list = Event.objects.order_by("-pub_date")[:5]
     context = {"latest_event_list": latest_event_list}
-    return render(request, "polls/index.html", context)
+    return render(request, "minamax/index.html", context)
 
     # def index(request):
     latest_event_list = Event.objects.order_by("-pub_date")[:5]
@@ -18,7 +18,7 @@ def index(request):
 
 def detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
-    return render(request, "polls/detail.html", {"event": event})
+    return render(request, "minamax/detail.html", {"event": event})
 
 
 def results(request, event_id):
