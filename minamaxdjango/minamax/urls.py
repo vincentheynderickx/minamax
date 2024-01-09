9f4d6cb5a3a8794caec3c4eb8ab464ec5449aef1
Olivier Fondeur <fondeurolivier14@gmail.com>
2024-01-09 16:29:35 +0100

    SC

--- minamaxdjango/minamax/urls.py
+++ minamaxdjango/minamax/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:event_id>/", views.detail, name="detail"),
-    path("<int:event_id>/results/", views.results, name="results"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
 ]
